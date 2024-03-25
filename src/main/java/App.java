d87f3c178a140a1e526c5b4ad07ed744940336be
Victor Rachev <waterlou5@mail.ru>
2024-03-25 17:12:27 +0300

    сделал

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat == beanCat2);
     }
 }
